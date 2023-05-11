3c615aada965a88233205d36748df60a48a74b4d
Sreekant Patnaik <132185254+Sree2k3@users.noreply.github.com>
2023-05-11 12:05:44 +0530

    Update M1 Cost Function.py

--- M1 Cost Function.py
+++ M1 Cost Function.py
@@ -7,10 +7,9 @@
 
 
 import numpy as np
-get_ipython().run_line_magic('matplotlib', 'widget')
 import matplotlib.pyplot as plt
 from lab_utils_uni import plt_intuition, plt_stationary, plt_update_onclick, soup_bowl
-plt.style.use('./deeplearning.mplstyle')
+plt.style.use('deeplearning.mplstyle')
 
 
 # In[10]:
